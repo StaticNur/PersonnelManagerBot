e75016e5af87d3e041e1abfa739999f6a0b0c84b
Mukhammadnur <gsjxne@gmail.com>
2023-09-10 15:21:36 +0300

    creat

---
+++ src/main/java/com/codemastersTournament/PersonnelManagerBot/PersonnelManagerBotApplication.java
@@ -0,0 +1,13 @@
+package com.codemastersTournament.PersonnelManagerBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonnelManagerBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonnelManagerBotApplication.class, args);
+	}
+
+}
